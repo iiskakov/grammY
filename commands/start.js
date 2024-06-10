0bd41feb0fb3591ed75b0b5be1bd0ca4cf449603
0x3005 <tradeshells@gmail.com>
2024-06-10 18:31:34 +0500

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -5,7 +5,7 @@ export default {
   example: "/start",
   category: "Utilities",
   handler: async (ctx) => {
-    const imageUrl = "https://raw.githubusercontent.com/iiskakov/grammY/start.jpg"; // Replace with the actual raw URL of your image
+    const imageUrl = "https://raw.githubusercontent.com/iiskakov/grammY/start.png"; // Replace with the actual raw URL of your image
     await ctx.replyWithPhoto(imageUrl, {
       caption: "Чтобы пользоваться ботом для тренировок продуктивно, нажми на «как проходить тренировку»\n\nКак проходить тренировку?\n Тренируйтесь 6 дней в неделю, а в воскресенье отдыхайте и устраивайте полный информационный детокс.\n Чтобы было хорошее настроение, память и продуктивность дайте мозгу восстановиться."
     });
