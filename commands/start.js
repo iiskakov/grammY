8ec664c5ce5bf8aea0e624234ac2575730153f4b
0x3005 <tradeshells@gmail.com>
2024-06-10 19:58:25 +0500

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -5,9 +5,9 @@ export default {
   example: "/start",
   category: "Utilities",
   handler: async (ctx) => {
-    const imageUrl = "https://res.cloudinary.com/dekfylowi/image/upload/v1718026658/g9wh8vpj6mrt6zq502h4.png"; // Replace with the actual raw URL of your image
+    const imageUrl = "https://res.cloudinary.com/dekfylowi/image/upload/v1718031445/Asset_3_ng87dg.png"; // Replace with the actual raw URL of your image
     await ctx.replyWithPhoto(imageUrl, {
-      caption: "Чтобы пользоваться ботом для тренировок продуктивно, нажми на «как проходить тренировку»\n\nКак проходить тренировку?\n Тренируйтесь 6 дней в неделю, а в воскресенье отдыхайте и устраивайте полный информационный детокс.\n Чтобы было хорошее настроение, память и продуктивность дайте мозгу восстановиться."
+      caption: "Как проходить тренировку?\n Тренируйтесь 6 дней в неделю, а в воскресенье отдыхайте и устраивайте полный информационный детокс.\n Чтобы было хорошее настроение, память и продуктивность дайте мозгу восстановиться."
     });
   },
 };
