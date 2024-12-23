5fddb987b7b1fcaa7292ef62c68014d5cc9ed7f1
Iskander <tradeshells@gmail.com>
2024-12-23 09:10:57 +0500

    voice

--- commands/start.js
+++ commands/start.js
@@ -1,3 +1,5 @@
+import { InputFile } from "grammy";
+
 export default {
   name: "start",
   description: "Start the bot",
@@ -5,9 +7,14 @@ export default {
   example: "/start",
   category: "Utilities",
   handler: async (ctx) => {
-    const imageUrl = "https://res.cloudinary.com/dekfylowi/image/upload/v1718031445/Asset_3_ng87dg.png"; // Replace with the actual raw URL of your image
+    const imageUrl = "https://res.cloudinary.com/dekfylowi/image/upload/v1718031445/Asset_3_ng87dg.png";
+    
+    // First, send the image with caption
     await ctx.replyWithPhoto(imageUrl, {
       caption: "Как проходить тренировку?\n Тренируйтесь 6 дней в неделю, а в воскресенье отдыхайте и устраивайте полный информационный детокс.\n Чтобы было хорошее настроение, память и продуктивность дайте мозгу восстановиться!"
     });
+
+    // Then, send the voice message
+    await ctx.replyWithVoice(new InputFile("./assets/1.ogg"));
   },
 };
