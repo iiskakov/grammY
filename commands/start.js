1fda53712c08ca7931751180ee62d37c81a53478
0x3005 <tradeshells@gmail.com>
2024-12-23 17:22:04 +0500

    Update start.js test

--- commands/start.js
+++ commands/start.js
@@ -7,13 +7,6 @@ export default {
   example: "/start",
   category: "Utilities",
   handler: async (ctx) => {
-    const imageUrl = "https://res.cloudinary.com/dekfylowi/image/upload/v1718031445/Asset_3_ng87dg.png";
-    
-    // First, send the image with caption
-    await ctx.replyWithPhoto(imageUrl, {
-      caption: "Как проходить тренировку?\n Тренируйтесь 6 дней в неделю, а в воскресенье отдыхайте и устраивайте полный информационный детокс.\n Чтобы было хорошее настроение, память и продуктивность дайте мозгу восстановиться!"
-    });
-
     // Send first voice message
     await ctx.replyWithVoice(new InputFile("./assets/1.ogg"));
     
