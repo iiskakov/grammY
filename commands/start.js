e26d740263038095fa9a60b79bfe0dac43d25674
0x3005 <tradeshells@gmail.com>
2025-10-01 15:31:03 +0500

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -10,8 +10,7 @@ export default {
     // Send first voice message
     await ctx.replyWithVoice(new InputFile("./assets/1.ogg"));
     
-    // Send text message and second voice message
-    await ctx.reply("Прослушайте второе аудио после того как прошли тест.");
+    
     await ctx.replyWithVoice(new InputFile("./assets/2.ogg"));
   },
 };
